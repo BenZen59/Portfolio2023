a0227657a6042811afa32b7b45ada03ca515abcc
benoit <benoit.zenone@gmail.com>
2023-04-22 22:00:33 +0200

    add link linkedin and github

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,10 +22,10 @@ export default function Header() {
         <Link to='/contact' className='ml-[20px]'>
           Contact
         </Link>
-        <button>
+        <button onClick={()=>{window.open('https://www.linkedin.com/in/beno%C3%AEt-zenone-7763aab1/', '_blank')}}>
           <BsLinkedin className='ml-[20px] w-[25px] h-[25px] mt-[-3px] mr-[20px]' />
         </button>
-        <button>
+        <button onClick={()=>{window.open('https://github.com/BenZen59', '_blank')}}>
           <BsGithub className='w-[25px] h-[25px] mt-[-3px] mr-[20px]' />
         </button>
       </div>
