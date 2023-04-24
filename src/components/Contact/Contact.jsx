ee2b072d3eee62bc0d035d1d849cff9d4db2733e
BenZen59 <benoit.zenone@gmail.com>
2023-04-24 10:35:02 +0200

    add contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import Moi from '../../img/photomoi.png';
+import { AiOutlineMail, AiFillMobile } from 'react-icons/ai';
+import { MdLocationPin } from 'react-icons/md';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 function Contact() {
-  return <div></div>;
+  return (
+    <div className='bg-zinc-800 px-20 py-20 rounded-lg border-solid border-2 border-gray-500 m-[25%] w-[423px] h-[284px] text-gray-400 font-mono mx-auto mt-[260px]'>
+      <h4 className='absolute text-gray-400 text-[48px] font-bold ml-[-60px] mt-[-70px]'>
+        ZENONE Benoît
+      </h4>
+      <div className='flex ml-[-65px]'>
+        <img src={Moi} alt='moi' className='w-[162px] h-[162px] mt-[10px]' />
+        <p className='mt-[25px] ml-[10px] text-[16px]'>
+          Développeur full stack
+          <br />
+          <div className='flex'>
+            <AiOutlineMail className='mt-[4px]' />{' '}
+            <span className='ml-[10px]'>benoit.zenone@gmail.com</span>
+          </div>
+          <div className='flex'>
+            <AiFillMobile className='mt-[4px]' />{' '}
+            <span className='ml-[10px]'>+33 7 81 70 95 11</span>
+          </div>
+          <div className='flex'>
+            <MdLocationPin className='mt-[4px]' />{' '}
+            <span className='ml-[10px]'>59000 LILLE</span>
+          </div>
+          <div className='mt-[10px] ml-[-15px]'>
+            <button
+              onClick={() => {
+                window.open(
+                  'https://www.linkedin.com/in/beno%C3%AEt-zenone-7763aab1/',
+                  '_blank'
+                );
+              }}
+            >
+              <BsLinkedin className='ml-[20px] w-[25px] h-[25px] mt-[-3px] mr-[20px]' />
+            </button>
+            <button
+              onClick={() => {
+                window.open('https://github.com/BenZen59', '_blank');
+              }}
+            >
+              <BsGithub className='w-[25px] h-[25px] mt-[-3px] mr-[20px]' />
+            </button>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
